4bb9e078a43e11ccd4ae35dddcc2a50962c9e833
Oriol Vall <oriol.vall.hdez@gmail.com>
2024-05-31 18:41:52 +0200

    chore: solve prospector errors

--- src/books/router.py
+++ src/books/router.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Depends
+
 from database import get_session
-from .service import Service
-from . import models
+
+from . import models, service
 
 router = APIRouter(prefix="/books", tags=["Books"])
 
 
 @router.get("/", response_model=list[models.Book])
 def get(db=Depends(get_session)):
-    return Service.get(db)
+    return service.get(db)
 
 
 @router.post("/", response_model=models.Book)
 def post(payload: models.BookBase, db=Depends(get_session)):
-    return Service.create(db, payload)
+    return service.create(db, payload)
--- src/books/service.py
+++ src/books/service.py
@@ -2,16 +2,14 @@ from sqlmodel import select
 from . import models
 
 
-class Service:
-    @classmethod
-    def get(cls, db):
-        return db.execute(select(models.Book)).all()
+def get(db):
+    return db.execute(select(models.Book)).all()
 
-    @classmethod
-    def create(cls, db, payload: models.BookBase):
-        db_book = models.Book.model_validate(payload)
 
-        db.add(db_book)
-        db.flush()
+def create(db, payload: models.BookBase):
+    db_book = models.Book.model_validate(payload)
 
-        return db_book
+    db.add(db_book)
+    db.flush()
+
+    return db_book

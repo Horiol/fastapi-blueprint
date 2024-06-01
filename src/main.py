9018f683cb4ab3144b79e8b5c32b3b6d6cafe37c
Oriol Vall <oriol.vall.hdez@gmail.com>
2024-06-01 17:13:31 +0200

    feat: add sqlalchemy admin library

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from src.books.router import router as books_router
+from src.admin import init_admin_page
 
 app = FastAPI()
 
@@ -12,6 +13,8 @@ async def healthz() -> dict[str, str]:
 
 
 app.include_router(books_router)
+
+init_admin_page(app)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)

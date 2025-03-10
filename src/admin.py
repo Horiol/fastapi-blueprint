aff60b460cf4a99309328c29dfcd706cb450121d
Horiol <oriol.vall.hdez@gmail.com>
2025-03-10 20:45:31 +0100

    feat: enhance admin book view with advanced features
    - Add column for book author
    - Enable search and sorting for title and author
    - Implement custom action method for books
    - Disable delete functionality
    - Add custom action with confirmation and redirection

--- src/admin.py
+++ src/admin.py
@@ -1,4 +1,6 @@
-from sqladmin import Admin, ModelView
+from fastapi.responses import RedirectResponse
+from sqladmin import Admin, ModelView, action
+from fastapi import Request
 from src.database import engine
 from src.books.models import Book
 
@@ -8,6 +10,28 @@ def init_admin_page(app):
 
     class AdminBooks(ModelView, model=Book):
         icon = "fa-solid fa-book"
-        column_list = [Book.id, Book.title]
+        can_delete = False
+        column_list = [Book.id, Book.title, Book.author]
+        column_searchable_list = [Book.title, Book.author]
+        column_sortable_list = [Book.id, Book.title, Book.author]
+
+        @action(
+            name="custom_action",
+            label="Custom action",
+            confirmation_message="Are you sure?",
+            add_in_detail=True,
+            add_in_list=True,
+        )
+        async def approve_users(self, request: Request):
+            if pks := request.query_params.get("pks", "").split(","):
+                for pk in pks:
+                    model: Book = await self.get_object_for_details(pk)
+                    print(model)
+
+            referer = request.headers.get("Referer")
+            if referer:
+                return RedirectResponse(referer)
+            else:
+                return RedirectResponse(request.url_for("admin:list", identity=self.identity))
 
     admin.add_view(AdminBooks)
